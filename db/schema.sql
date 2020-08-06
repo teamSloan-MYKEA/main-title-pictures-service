2e9d48743a561fbe96724e3271fd622fbbee9f04
manzaloros <zacharymansellcello@gmail.com>
2020-08-06 12:42:52 -0700

    Add schema for DB

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS mykea_main_title_pictures;
+
+CREATE DATABASE mykea_main_title_pictures;
+
+USE mykea_main_title_pictures;
+
+
+CREATE TABLE descriptions (
+  id INT NOT NULL AUTO_INCREMENT,
+  description VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE pictures (
+  id INT NOT NULL AUTO_INCREMENT,
+  url TEXT,
+  description_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (description_id) REFERENCES descriptions (id)
+);
