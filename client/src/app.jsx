238da639dc3e6abbe1018552e9e573adadaf8043
manzaloros <zacharymansellcello@gmail.com>
2020-08-08 11:55:10 -0700

    Add app.jsx and index.jsx initial code

---
+++ client/src/app.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello, World!</div>
+    );
+  }
+}
