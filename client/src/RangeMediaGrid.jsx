cec1f91f9d8b16c9e254388cfdcd86a74a93e120
manzaloros <zacharymansellcello@gmail.com>
2020-08-10 09:45:01 -0700

    Create MediaContainer, RangeMediaGrid, RangeRevamp Button, RangeRevampMediaGrid components, render a hello world

---
+++ client/src/RangeMediaGrid.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RangeMediaGrid() {
+  return <div>Hello from RangeMediaGrid</div>;
+}
+
+export default RangeMediaGrid;
--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import RangeMediaGrid from './RangeMediaGrid.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +8,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>Hello, World!</div>
+      <RangeMediaGrid />
     );
   }
 }
