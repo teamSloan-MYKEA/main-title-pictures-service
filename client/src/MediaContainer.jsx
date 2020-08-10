268c170464212a4056d25779dc45fcb3265897a7
manzaloros <zacharymansellcello@gmail.com>
2020-08-10 12:11:41 -0700

    Add image url and src tags to render

--- client/src/MediaContainer.jsx
+++ client/src/MediaContainer.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function MediaContainer({ image }) {
-  return <div>{image}</div>;
+  return (
+    <div>
+      <img src={image} alt="IKEA furniture">
+    </div>
+  );
 }
 MediaContainer.propTypes = {
-  image: PropTypes.instanceOf(Object).isRequired,
+        image: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default MediaContainer;
