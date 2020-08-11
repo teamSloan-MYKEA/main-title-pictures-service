8e35371bfb471e40433ba1a1ae41e1221e80404b
manzaloros <zacharymansellcello@gmail.com>
2020-08-11 15:34:03 -0700

    Uninstall free font Awesome, install react ver

--- client/src/Modal/LeftArrow.jsx
+++ client/src/Modal/LeftArrow.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fa-arrow - left } from '@fortawesome/free-solid-svg-icons';
+
 
 function LeftArrow() {
   return (
