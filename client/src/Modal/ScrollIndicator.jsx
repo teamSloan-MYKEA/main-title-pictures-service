ddaed7e71e1708d56e522d0db0fa9aa17fc61c26
manzaloros <zacharymansellcello@gmail.com>
2020-08-15 09:51:38 -0700

    Add scaleX translateX to test scroll bar

--- client/src/Modal/ScrollIndicator.jsx
+++ client/src/Modal/ScrollIndicator.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { ScrollIndicatorBar, ScrollIndicatorBarWrapper, ScrollIndicatorButton } from '../Styles';
 
 function ScrollIndicator({ activeIndex, slideDirection }) {
+
+  const ScrollIndicatorBar = styled.span`
+  will-change: transform;
+  background: #111;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  -webkit-transform-origin: 0 0;
+  -ms-transform-origin: 0 0;
+  transform: scaleX(0.11) translateX(200%);
+  transform-origin: 0 0;
+  transition: 5s;
+  display: block;
+`;
   return (
     <ScrollIndicatorButton>
       <ScrollIndicatorBarWrapper>
+        {/* <ScrollIndicatorBar /> */}
         <ScrollIndicatorBar />
       </ScrollIndicatorBarWrapper>
     </ScrollIndicatorButton>
