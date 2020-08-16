64913e7fa4efb3e1a5cc70aee4260732b2f8e739
manzaloros <zacharymansellcello@gmail.com>
2020-08-16 12:36:57 -0700

    Add static in app.use

--- server/index.js
+++ server/index.js
@@ -5,14 +5,23 @@ const db = require('../db/index.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '..', '/client', '/dist')));
+// app.use(express.static(path.join(__dirname, '..', '/client', '/dist')));
 
-// Use express params
-app.get('/:id', (req, res) => {
+app.use('/:id', (req, res) => {
   db.getPictures(req.params.id)
-    .then((urls) => res.send(urls))
+    .then(((urls) => {
+      res.send(urls);
+      express.static(path.join(__dirname, '..', '/client', '/dist'));
+    }))
     .catch(() => res.status(500).send('Internal Server Error'));
 });
+
+// Use express params
+// app.get('/:id', (req, res) => {
+//   db.getPictures(req.params.id)
+//     .then((urls) => res.send(urls))
+//     .catch(() => res.status(500).send('Internal Server Error'));
+// });
 
 app.listen(port, () => {
   console.log(`Main-title-pictures-service listening at http://localhost:${port}`);
