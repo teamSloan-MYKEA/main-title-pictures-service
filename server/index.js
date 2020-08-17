7ac929f1c314af33e0530522897951657bac156a
manzaloros <zacharymansellcello@gmail.com>
2020-08-17 09:23:43 -0700

    Try new dynamic endpoint code in server / app

--- server/index.js
+++ server/index.js
@@ -5,23 +5,15 @@ const db = require('../db/index.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname, '..', '/client', '/dist')));
-
-app.use('/:id', (req, res) => {
-  db.getPictures(req.params.id)
-    .then(((urls) => {
-      express.static(path.join(__dirname, '..', '/client', '/dist'));
-      res.send(urls);
-    }))
-    .catch(() => res.status(500).send('Internal Server Error'));
-});
+app.use('/', express.static(path.join(__dirname, '..', '/client', '/dist')));
 
 // Use express params
-// app.get('/:id', (req, res) => {
-//   db.getPictures(req.params.id)
-//     .then((urls) => res.send(urls))
-//     .catch(() => res.status(500).send('Internal Server Error'));
-// });
+app.get('/:id', (req, res) => {
+  console.log('REcieved request')
+  db.getPictures(req.params.id)
+    .then((urls) => res.send(urls))
+    .catch(() => res.status(500).send('Internal Server Error'));
+});
 
 app.listen(port, () => {
   console.log(`Main-title-pictures-service listening at http://localhost:${port}`);
