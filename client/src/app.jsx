8c6fde8b66e2e42573da7f50e3ac346a3fc616a3
manzaloros <zacharymansellcello@gmail.com>
2020-08-19 10:31:08 -0700

    Remove styles reset

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Reset } from 'styled-reset';
 import RangeMediaGrid from './RangeMediaGrid';
 import Modal from './Modal/ModalDashboard';
 
@@ -64,7 +63,6 @@ class App extends Component {
     } = this.state;
     return (
       <div>
-        {/* <Reset /> */}
         <RangeMediaGrid
           images={
             isCollapsed ? images.slice(0, 4) : images
