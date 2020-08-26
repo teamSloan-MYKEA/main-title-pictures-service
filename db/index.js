f88d5a2f9ad78272aeee50cd76ff70619743f285
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-26 14:41:09 -0500

    API endpoints get, put, and post working

--- db/index.js
+++ db/index.js
@@ -19,27 +19,41 @@ const getPictures = (id) => new Promise((resolve, reject) => {
   });
 });
 
-// below is just copied, but represents the post function
-// needs to be switched to insert into table_name (cols) values (vals)
-const addPicture = (id) => new Promise((resolve, reject) => {
-  const sql = `SELECT * FROM pictures p INNER JOIN descriptions d
-              ON p.description_id = ${id} AND
-              p.description_id = d.id LIMIT 6`;
-  connection.query(sql, (err, results) => {
+// insert into table_name (cols) values (vals)
+const addItem = (item) => new Promise((resolve, reject) => {
+  const {picture1, picture2, picture3, picture4, picture5, picture6, description} = item;
+  const pictureArray = [picture1, picture2, picture3, picture4, picture5, picture6];
+  const sql1 = `INSERT INTO descriptions(description) VALUES(?)`;
+  const sql15 = `SELECT LAST_INSERT_ID()`;
+  const sql2 = `INSERT INTO pictures (url, description_id) VALUES (?, ?)`;
+  connection.query(sql1, [description], (err, results) => {
     if (err) {
       return reject(err);
     }
-    return resolve(results);
+    connection.query(sql15, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      const lastInsertId = results[0]['LAST)_INSERT_ID()'];
+      for (var i = 0; i < pictureArray.length - 1; i++) {
+        connection.query(sql2, [pictureArray[i], results[0]['LAST_INSERT_ID()']], (err, results) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(results);
+        });
+      }
+    });
   });
 });
 
-// below is just copied, but represents the put function
 // update table_name set col=val where col=val
-const updatePicture = (id) => new Promise((resolve, reject) => {
-  const sql = `SELECT * FROM pictures p INNER JOIN descriptions d
-              ON p.description_id = ${id} AND
-              p.description_id = d.id LIMIT 6`;
-  connection.query(sql, (err, results) => {
+const updateItem = (item) => new Promise((resolve, reject) => {
+  const {table, id, update} = item;
+  const field = table === 'descriptions' ? 'description' : 'url';
+  const sql1 = `UPDATE ${table} SET ${field}="${update}" WHERE id=${id}`;
+  console.log(sql1);
+  connection.query(sql1, (err, results) => {
     if (err) {
       return reject(err);
     }
@@ -49,7 +63,7 @@ const updatePicture = (id) => new Promise((resolve, reject) => {
 
 // below is just copied, but represents the delete function
 // update to delete from table_name where col=val
-const deletePicture = (id) => new Promise((resolve, reject) => {
+const deleteItem = (id) => new Promise((resolve, reject) => {
   const sql = `SELECT * FROM pictures p INNER JOIN descriptions d
               ON p.description_id = ${id} AND
               p.description_id = d.id LIMIT 6`;
@@ -63,3 +77,6 @@ const deletePicture = (id) => new Promise((resolve, reject) => {
 
 module.exports.connection = connection;
 module.exports.getPictures = getPictures;
+module.exports.addItem = addItem;
+module.exports.updateItem = updateItem;
+module.exports.deleteItem = deleteItem;
