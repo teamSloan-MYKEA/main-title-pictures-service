7b0fae24464d7defbafb6a6427eea1d59e84ae12
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-29 16:45:44 -0500

    10 million records seeded

--- db/seedPostgres2.js
+++ db/seedPostgres2.js
@@ -7,17 +7,11 @@ const client = new Client({
   database: "sdc_mykea"
 });
 
-var copyInsert = function(fileCount) {
-  client.connect();
-  client.query(
-    `COPY pictures_service (description, picture1, picture2, picture3, picture4, picture5, picture6) FROM '/var/lib/postgresql/data/pictures${fileCount}.csv' WITH (format csv)`,
-    (err, res) => {
-      console.log(err ? err.stack : 'table pictures_service up and seeded');
-      client.end();
-    }
-  );
-}
-
-module.exports = {
-  copyInsert
-};
+client.connect();
+client.query(
+  `COPY pictures_service (description, picture1, picture2, picture3, picture4, picture5, picture6) FROM '/var/lib/postgresql/data/pictures.csv' WITH (format csv)`,
+  (err, res) => {
+    console.log(err ? err.stack : 'table pictures_service up and seeded seedPostgres2');
+    client.end();
+  }
+);
