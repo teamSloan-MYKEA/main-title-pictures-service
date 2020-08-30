71b7abfef72b83bd7b15940506e99685b20ed33c
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-29 19:12:33 -0500

    Cassandra seeding working within cqlsh, scripting

---
+++ db/seedCassandra.js
@@ -0,0 +1,38 @@
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({
+  contactPoints: ['localhost'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'sdc_mykea'
+});
+
+const query1 = `DROP TABLE IF EXISTS sdc_mykea.pictures_service;`
+const query2 = `CREATE TABLE IF NOT EXISTS sdc_mykea.pictures_service (
+    description text,
+    picture1 text,
+    picture2 text,
+    picture3 text,
+    picture4 text,
+    picture5 text,
+    picture6 text,
+    id int primary key
+  );`;
+const query3 = `COPY sdc_mykea.pictures_service (description, picture1, picture2, picture3, picture4, picture5, picture6, default) FROM '/var/lib/cassandra/pictures.csv';`;
+
+client.connect()
+  .then(() => console.log('success'))
+  .catch((err) => console.log(err))
+  .then(() => {
+    client.execute(query1)
+      .then(() => {
+        console.log('query1 in');
+        client.execute(query2)
+          .then(() => {
+            console.log('query2 in');
+            client.execute(query3)
+              .then(() => {
+                console.log('three queries executed, shutting down');
+                client.shutdown();
+              }).catch((err) => console.log(err));
+          }).catch((err) => console.log(err));
+      }).catch((err) => console.log(err))
+    }).catch((err) => console.log(err));
